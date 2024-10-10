5d175fb5e906558520474849077de0d4a2224cf9
harimeghanath <harimeghanath@gmail.com>
2024-10-10 05:13:27 +0000

    SDN

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
+import pandas as pd
+#import matplotlib.pyplot as plt
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Simple Data Dashboard")
+
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+                                 
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+
+    st.subheader("Data Preview")  
+    st.write(df.head())                         
+
+    st.subheader("Data Summary")  
+    st.write(df.describe())                         
+
+    st.subheader('Filter Data')
+    columns = df.columns.tolist()
+    selected_column = st.selectbox("Select Column to filter by", columns)
+    unique_values = df[selected_column].unique()
+    selected_value = st.selectbox("Select value", unique_values) 
+
+    filtered_df = df[df[selected_column] == selected_value] 
+    st.write(filtered_df)
